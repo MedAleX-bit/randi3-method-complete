29665d01fea7e111b181b7db8e778b1a7435ba62
dschrimpf <ds@randi2.de>
2012-08-29 14:59:02 +0200

    changed randomization plugin structure

--- src/main/scala/org/randi3/randomization/CompleteRandomizationPlugin.scala
+++ src/main/scala/org/randi3/randomization/CompleteRandomizationPlugin.scala
@@ -4,7 +4,9 @@ import org.randi3.randomization._
 import org.randi3.randomization.configuration._
 import org.randi3.dao.CompleteRandomizationDao
 import org.randi3.model._
-
+import org.randi3.model.criterion.Criterion
+import org.randi3.model.criterion.constraint.Constraint
+import criterion.Criterion
 import org.scalaquery.ql._
 import org.scalaquery.ql.extended.ExtendedProfile
 import org.scalaquery.session.Database
@@ -14,18 +16,25 @@ import scalaz._
 
 class CompleteRandomizationPlugin(database: Database, driver: ExtendedProfile) extends RandomizationMethodPlugin(database, driver) {
 
-  val name = CompleteRandomization.getClass().getName().substring(0,  CompleteRandomization.getClass().getName().size - 1)
+  val name = CompleteRandomization.getClass.getName.substring(0,  CompleteRandomization.getClass.getName.size - 1)
 
   val i18nName = name
 
-  val completeRandomizationDao = new CompleteRandomizationDao(database, driver)
+  val description = "Simple complete randomization method (ignores stratification options and different treatment proportions)"
 
-  def randomizationConfigurations(): List[ConfigurationType[Any]] = {
-    List()
+  val canBeUsedWithStratification = false
+
+  private val completeRandomizationDao = new CompleteRandomizationDao(database, driver)
+
+  def randomizationConfigurationOptions(): (List[ConfigurationType[Any]], List[Criterion[_ <: Any, Constraint[_ <: Any]]])= {
+   (Nil, Nil)
+  }
+
+  def getRandomizationConfigurations: List[ConfigurationProperty[Any]] = {
+    Nil
   }
 
   def randomizationMethod(random: RandomGenerator, trial: Trial, configuration: List[ConfigurationProperty[Any]]): Validation[String, RandomizationMethod] = {
-    //TODO check parameter
     Success(new CompleteRandomization(random = random))
   }
 
